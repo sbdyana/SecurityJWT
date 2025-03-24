6addb9741eb8c6176fe2b0ac3c4c2a3dad18205e
sbdyana <tnqls9301@naver.com>
2025-03-24 13:55:03 +0900

    회원가입 구현

--- src/main/java/com/cos/securityjwt/controller/JoinController.java
+++ src/main/java/com/cos/securityjwt/controller/JoinController.java
@@ -1,16 +1,23 @@
 package com.cos.securityjwt.controller;
 
 import com.cos.securityjwt.dto.JoinDTO;
+import com.cos.securityjwt.service.JoinService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody // api 서버를 만들거기 때문에 응답을 responsbody로 해야함.
+@RequiredArgsConstructor
 public class JoinController {
+
+    private final JoinService joinService;
 
     @PostMapping("/join")
     public String joinProcess(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
         return "ok";
     }
 }
--- src/main/java/com/cos/securityjwt/service/JoinService.java
+++ src/main/java/com/cos/securityjwt/service/JoinService.java
@@ -4,6 +4,7 @@ import com.cos.securityjwt.dto.JoinDTO;
 import com.cos.securityjwt.entity.UserEntity;
 import com.cos.securityjwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service // 스프링에게 관리되도록 선언
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class JoinService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 //    public JoinService(UserRepository userRepository) {
 //        this.userRepository = userRepository;
 //    }
@@ -29,7 +31,10 @@ public class JoinService {
 
         UserEntity data = new UserEntity();
         data.setUsername(username);
-        data.setPassword(password);
-        // data.setRole();
+        data.setPassword(bCryptPasswordEncoder.encode(password)); // 무조건 암호화 진행해서 넣어야 함 > BCryptPasswordEncoder 빈으로 등록해놨기 때문에 주입받아서 쓸 것임
+        data.setRole("ROLE_ADMIN"); // "ROLE_ 은 고정이고 그 뒤에 원하는 권한
+        // 회원 정보 저장
+
+        userRepository.save(data);
     }
 }
